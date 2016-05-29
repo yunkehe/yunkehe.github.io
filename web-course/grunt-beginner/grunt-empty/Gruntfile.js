af8b8716ba2499646c749084f1cca5dde03d4b2a
yunkehe <yunkehe@163.com>
2016-05-29 19:25:54 +0800

    grunt copy and clean

---
+++ web-course/grunt-beginner/grunt-empty/Gruntfile.js
@@ -0,0 +1,61 @@
+// Generated on 2016-05-27 using generator-angular 0.15.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths for the application
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    config : config,
+
+    // 复制文件
+    copy : {
+      // target
+      dist_html : {
+        files : [{
+                  expand : true,
+                  cwd : '<%= config.app %>/',
+                  src : '*.html',
+                  dest : '<%= config.dist %>/',
+                  ext : '.mim.html',
+                  extDot : 'first',
+                  flatten : true,
+                  rename : function(dest, src){
+                    return dest + 'js/' + src;
+                  }
+                }]
+      }
+    },
+
+    // 删除文件
+    clean : {
+      dist : {
+        // 被清除文件路径
+        src : '<%= config.dist%>/**/*',
+        // 只清除文件
+        filter : 'isFile',
+        // filter : function(filePath){
+        //   return !grunt.file.isDir(filePath);
+        // }
+      }
+    }
+   
+  });
+  
+};
