85174e31ff43f74ef26c78450c31e88babf1f4d1
何珂 <yunkehe@163.com>
2016-12-21 17:21:23 +0800

    index.html

--- js/main.js
+++ js/main.js
@@ -40,15 +40,137 @@ function init(id) {
 		}
 
 		document.querySelector('.tab-content.'+flag).style.display = 'block';
+		if(flag == 'about'){
+			canvasObj.wrapper.style.display = 'none';
+		}else{
+			canvasObj.wrapper.style.display = 'block';
+		}
 	}
 
 	for(var i=0; i<jumpBtn.length; i++){
 		jumpBtn[i].addEventListener('click', handler);
 	}
 
-
 };
 
 // 初始化
 init('pro-list');
-// 切换效果
+
+function createCanvas(){
+
+	var canvasWrapper = document.getElementById('canvas-wrapper'),
+		bRect = canvasWrapper.getBoundingClientRect();
+
+		console.log('params', bRect)
+
+	var myCanvas = document.createElement("canvas");
+		myCanvas.setAttribute("width", bRect.width - 17 );
+		myCanvas.setAttribute("height", bRect.height - 17);
+		myCanvas.setAttribute("id", "myCanvas");
+		canvasWrapper.appendChild(myCanvas);	
+
+	var mycanvas = document.getElementById("myCanvas");
+
+	return 	{
+		wrapper: canvasWrapper,
+		mycanvas: mycanvas,
+		ctx: mycanvas.getContext('2d'),
+		maxWidth: mycanvas.width,
+		maxHeight: mycanvas.height
+	};
+}
+var colors = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"];
+
+// 动态创建canvas;
+var canvasObj = createCanvas();
+
+function drawCircle(x, y, radius, color){
+	var ctx = canvasObj.ctx;
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, Math.PI*2);
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.closePath();
+}
+
+// 返回max到min之间的整随机数
+function getRandom(max, min){
+	return Math.floor( Math.random()*(max-min)+min );
+}
+
+
+function Ball(){
+	this.forwardX = true;
+	this.forwardY = true;
+
+	this.x = 400;
+	this.y = 300;
+	this.radius = getRandom(30, 10);
+	this.color = colors[getRandom(colors.length, 0)];
+
+	var speed = getRandom(8, 3);
+
+	// 根据角度计算随机方向
+	var direction = Math.random()*Math.PI*2;
+
+	this.moveX = Math.cos(direction)*speed;
+	this.moveY = Math.sin(direction)*speed;
+
+}
+
+Ball.prototype.move = function(){
+	var maxWidth = canvasObj.maxWidth,
+		maxHeight = canvasObj.maxHeight;
+
+	if(this.forwardX){
+		this.x += this.moveX;
+		if(this.x >= (maxWidth - this.radius) || this.x <= this.radius){
+			this.forwardX = false;
+		}
+
+	}else{
+		this.x -= this.moveX;
+		if(this.x >= (maxWidth - this.radius) || this.x <= this.radius){
+			this.forwardX = true;
+		}
+	}
+
+	if(this.forwardY){
+		this.y += this.moveY;
+		if(this.y >= (maxHeight - this.radius) || this.y <= this.radius ){
+			this.forwardY = false;
+		}
+	}else{
+		this.y -= this.moveY;
+		if(this.y >= (maxHeight - this.radius) || this.y <= this.radius){
+			this.forwardY = true;
+		}
+	}
+
+	drawCircle(this.x, this.y, this.radius, this.color);
+	
+}
+
+function start(){
+	var ctx = canvasObj.ctx,
+		mycanvas = canvasObj.mycanvas;
+
+	var balls = [];
+	for(var i=0; i<20; i++){
+		balls[i] = new Ball();
+	}
+
+	setInterval(function(){
+		ctx.clearRect(0, 0, mycanvas.width, mycanvas.height);
+
+		for(var j=0; j<balls.length; j++){
+			balls[j].move();
+		}
+
+	}, 20)
+
+}
+// 動畫效果
+start();
+
+
