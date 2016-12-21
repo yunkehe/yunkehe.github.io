a66307df8e07a555ff403bd3340aa0f2207064b1
何珂 <yunkehe@163.com>
2016-12-21 16:43:22 +0800

    index2.html

--- js/main.js
+++ js/main.js
@@ -1,9 +1,54 @@
-Function.prototype.method = function(name, func){
-	if( this.prototype[name] ){
-		return;
-	}else{
-		this.prototype[name] = func;
+/* 关于 作品链接 联系 */
+// 项目链接
+function init(id) {
+	// {
 
-		return this;
+	// 	'name': 'nodejs博客',
+	// 	'href': 'http://yunheke.tk:9000/'
+	// }
+	    projects = [{
+		'name': '诺亚教育平台教师PC端',
+		'href': 'http://e.anoah.com'
+	},{
+		'name': '诺亚教育平台学生PC端',
+		'href': 'http://e.anoah.com/ebags'
+	}];
+
+	var proList = document.getElementById(id);
+
+	for(var i=0, length=projects.length; i<length; i++){
+		var a = document.createElement('a'),
+		    li = document.createElement('li');
+		
+		a.href = projects[i].href;
+		a.innerText = projects[i].name;
+	
+		li.appendChild(a);
+		proList.appendChild(li);
 	}
-}
+
+	// tab切换...
+	var tabContent = document.querySelectorAll('.tab-content'),
+		jumpBtn = document.querySelectorAll('.jump-btn');
+
+	function handler(e){
+		var target = e.currentTarget;
+		var flag = target.getAttribute('flag');
+
+		for(var i=0; i<tabContent.length;i++){
+			tabContent[i].style.display = 'none';
+		}
+
+		document.querySelector('.tab-content.'+flag).style.display = 'block';
+	}
+
+	for(var i=0; i<jumpBtn.length; i++){
+		jumpBtn[i].addEventListener('click', handler);
+	}
+
+
+};
+
+// 初始化
+init('pro-list');
+// 切换效果
