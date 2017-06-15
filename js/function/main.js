638e75c7e4b999eb37884772d2de542a2fd1dbab
yunkehe <yunkehe@163.com>
2017-06-15 11:41:48 +0800

    parseHTML fun

--- js/function/main.js
+++ js/function/main.js
@@ -59,7 +59,34 @@ function pagination(){
 	 		}
 	 	};
 
+	// @param   id   容器id
+	// @param   tag  指定包裹每个文本的tag
+	// @param   isRender  处理完成后用新html渲染页面
+	this.parseHTMLByTag = function(id, tag, isRender){
+	    var content = document.getElementById(id);
+	    var tag = tag || 't';
+	    var hb = content.outerHTML;
 
+	    // var hb = document.body.outerHTML;
+	    var hb = hb.replace(/>([^<]+)</igm,'><z>$1<\/z><');
+	    //console.log('get Z',m);
+	     var parser = new DOMParser();
+	     var hb_dom = parser.parseFromString(hb,"text/html")
+	     //console.log('parser',d);
+	     var hb_dom_z = hb_dom.body.querySelectorAll('z');
+	     //console.log('dom Z',s);
+	     for(var i = 0;i<hb_dom_z.length;i++){
+	          hb_dom_z[i].innerHTML = hb_dom_z[i].innerText.replace(/(\S)/igm,'<'+tag+'>$1</'+tag+'>');
+	     }
+
+	     var new_hb = hb_dom.body.outerHTML.replace(/<(\/)?z>/igm,'');
+	     // document.body.outerHTML = new_hb;
+	    
+	    // 处理完成后渲染页面
+	    if( isRender ) content.outerHTML = new_hb;
+
+	    return new_hb;
+	};
 };
 
 /**
